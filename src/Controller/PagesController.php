cd292e38c5edb031d6e44251c2f02b7bc1b508f2
macbook <yvesomda93@yahoo.fr>
2021-05-06 22:09:34 +0000

    Ajout de la notion de service

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -6,22 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use DateTimeZone;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CalendarService;
 
 class PagesController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-        $today = new \DateTimeImmutable('now', new DateTimeZone('America/Montreal'));
-
+       
         //$isNewYear = $currentTime->format('d') == '01' and $currentTime->format('m') == '01';
-        $isNewYear = $today->format('z') == '0';
+        $CalendarService = new CalendarService();
 
-        $nextJanuaryFirst = new \DateTimeImmutable('1st January Next Year', new DateTimeZone('America/Montreal'));
+        return $this->render('pages/home.html.twig', [
+            'isNewYear' =>$CalendarService->isNewYear(),
+            'daysLeftUntilNextYear' => $CalendarService->daysLeftUntilNextYear()
 
-        $daysLeftUntilNextYear = $nextJanuaryFirst->diff($today)->days;
-
-        return $this->render('pages/home.html.twig', compact('isNewYear', 'daysLeftUntilNextYear'));
+                    ]);
     }
 
     #[Route('/about', name: 'app_about')]
---
+++ src/Service/CalendarService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+use DateTimeZone;
+use DateTimeInterface;
+use DateTimeImmutable;
+
+class CalendarService
+{
+
+
+	public function isNewYear(): bool
+	{
+		 return $this->today()->format('z') == '0';
+	}
+
+
+	public function daysLeftUntilNextYear(): int
+	{
+		$nextJanuaryFirst = new DateTimeImmutable('1st January Next Year', new DateTimeZone('America/Montreal'));
+
+        return $nextJanuaryFirst->diff($this->today())->days;
+	}
+
+	public function today(): DateTimeInterface
+	{
+		return new DateTimeImmutable('now', new DateTimeZone('America/Montreal'));
+	}
+}
