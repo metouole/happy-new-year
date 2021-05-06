fe8289506758203684662696f227e54073e45617
macbook <yvesomda93@yahoo.fr>
2021-05-06 21:43:50 +0000

    display number of days left before the end of the year

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -12,12 +12,16 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-        $currentTime = new \DateTimeImmutable('now', new DateTimeZone('America/Montreal'));
+        $today = new \DateTimeImmutable('now', new DateTimeZone('America/Montreal'));
 
         //$isNewYear = $currentTime->format('d') == '01' and $currentTime->format('m') == '01';
-        $isNewYear = $currentTime->format('z') == '0';
+        $isNewYear = $today->format('z') == '0';
 
-        return $this->render('pages/home.html.twig', compact('isNewYear'));
+        $nextJanuaryFirst = new \DateTimeImmutable('1st January Next Year', new DateTimeZone('America/Montreal'));
+
+        $daysLeftUntilNextYear = $nextJanuaryFirst->diff($today)->days;
+
+        return $this->render('pages/home.html.twig', compact('isNewYear', 'daysLeftUntilNextYear'));
     }
 
     #[Route('/about', name: 'app_about')]
